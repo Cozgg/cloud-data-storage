84eceb5a4c23ba074bf29f20b5624ad1cc55eea6
Cozg <nguyenhuucong295@gmail.com>
2025-11-21 23:25:38 +0700

    update config

--- data-storage-app/storageapp/__init__.py
+++ data-storage-app/storageapp/__init__.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 app.secret_key = "Cozgdeptraiokechuaae123@"
 
 # Thay 'PUBLIC_IP_CUA_EC2_MINIO' bằng IP thật trên AWS của bạn (ví dụ: 3.14.15.92)
-app.config["MINIO_ENDPOINT"] = "127.0.0.1:9000"
-app.config["MINIO_ACCESS_KEY"] = "minioadmin"
-app.config["MINIO_SECRET_KEY"] = "minioadmin"
+app.config["MINIO_ENDPOINT"] = "54.158.50.254:9000"
+app.config["MINIO_ACCESS_KEY"] = "admin"
+app.config["MINIO_SECRET_KEY"] = "matkhaucuaban123"
 app.config["MINIO_SECURE"] = False
 
 # --- CẤU HÌNH DATABASE (RDS HOẶC EC2 SỐ 2) ---
 # Thay 'ENDPOINT_CUA_RDS' bằng đường dẫn AWS cấp cho bạn
-DB_USER = "root"
-DB_PASS = "root"
-DB_HOST = "localhost" # Endpoint RDS
+DB_USER = "admin"
+DB_PASS = "12345678"
+DB_HOST = "database-cloud-storage.cuakz9khnlh0.us-east-1.rds.amazonaws.com" # Endpoint RDS
 DB_NAME = "cloudstorage"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
